50851207bf2c9f94d481ab0318f2df83441dc24f
Dizrtt <demonic2003@bk.ru>
2022-10-16 16:43:34 +0200

    4 lesson cases uploaded

---
+++ PJ11_Array/Program.cs
@@ -0,0 +1,52 @@
+﻿// используем генератор случайных чисел и методы
+Console.Clear();
+void FillArray(int[] collection) // FillArray - название метода, collection - название аргумента
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); // обратиться к аргументу collection на позицию indexи положить туда новое целое число от 1 до 10
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // "-1" принятый программистами прием, когда выводится "-1" когда элемента нет
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // создать новый массив в котором будет 10 элементов
+
+FillArray(array);
+PrintArray(array);
+array[4] = 4;
+array[6] = 4;
+Console.WriteLine();
+
+int pos = IndexOf(array, 43);
+Console.WriteLine(pos);
---
+++ PJ9_IntroArray/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача с масимумумом из 9 чисел
+Console.Clear();
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//               0   1   2   3   4    5   6   7   8
+int[] array = { 12, 32, 35, 24, 555, 36, 4227, 80, 39 };
+// array[0] = 13; // array[] - обращение к элементу массива с индексом, укаазнным в квадратных скобках
+//Console.Write(array[0]);
+
+ int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+Console.WriteLine(max);
+
+
